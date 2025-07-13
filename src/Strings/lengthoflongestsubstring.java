52419a577a6ef957565be6e12e1f9e33a2405cae
David Biffert <adambiffert@gmail.com>
2025-07-13 15:43:18 -0500

    Add initial Java templates and update .gitignore
    Added leetcode_java.iml for IntelliJ project configuration, new Java files for problem templates and test runners, and updated .gitignore to exclude .idea directory.

---
+++ src/Strings/lengthoflongestsubstring.java
@@ -0,0 +1,24 @@
+package Strings;
+
+class lengthoflongestsubstring {
+    public static int problem(String s) {
+
+        return 0;
+    }
+
+    // Custom test case runner
+    public static void main(String[] args) {
+        // Test cases
+        runTest("abcabcbb", 3);
+        runTest("bbbbb", 1);
+        runTest("pwwkew", 3);
+    }
+
+    static int runNumber = 0;
+    // Test runner (throws AssertionError if test fails)
+    private static void runTest(String param1, int expected) {
+        int result = problem(param1);
+        assert result == expected : String.format("❌ Failed: run %d", runNumber++);
+        System.out.println("✅ Passed");
+    }
+}
